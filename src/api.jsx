5ddbc9a4210f380248f29be6f64b2861a7489419
song-chaeyoung <153435389+song-chaeyoung@users.noreply.github.com>
2024-11-29 17:40:20 +0900

    first deploy

--- src/api.jsx
+++ src/api.jsx
@@ -1,4 +1,5 @@
-const API_BASE_URL = "http://localhost:4000/api/drugs";
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const API_BASE_URL = "/api/drugs";
 
 export const getAllDrugs = async () => {
   try {
